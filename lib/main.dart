94914f7d85de4d2088bd8c812fa284d886cfd8be
Mac <mac@Macs-MacBook-Pro.local>
2023-07-19 15:51:11 +0300

    test no dev

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Test CI/CD',
+              'Test CI/CD nod dev',
             ),
             Text(
               '$_counter',
